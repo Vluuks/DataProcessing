e69fac56056705ab090b2817d57f99a94a6954fa
Vluuks <vluuks@gmail.com>
2017-02-14 17:14:02 +0100

    Data processed in array, time im milliseconds and day offset added

--- Homework/week2/script.js
+++ Homework/week2/script.js
@@ -12,9 +12,42 @@ $$ |  $$ |\$$$$$$$\ $$ |  $$ |$$$$$$$  |$$ | \$$\ \$$$$$$$\
 Renske Talsma
 10896503
 		
-*/                                                            
+*/                                                             
                       
-getData();					  
+var dataArray = getData();
+var transformData = getDomainRange(dataArray);
+
+
+// Voor elk datapunt, vraag de bijbehorende y op?
+// Dit deel is nog niet echt duidelijk voor me
+var y = transformData(40);
+console.log("y test" + y);
+			  
+canvasTest();
+
+
+
+function getDomainRange(dataArray){
+    
+    // Minimum and maximum value of x-axis (time)
+    var domain = [];
+    domain[0] = dataArray[0][2];
+    domain[1] = dataArray[dataArray.length-1][2];
+  
+    console.log(domain);
+  
+    var range = [];
+    range[0] = -100;
+    range[1] = 100;
+    
+    console.log(range);
+    
+    // Create transform function with these values
+    return createTransform(domain, range);
+    
+}
+
+
 
 /* Retrieves data from the HTML element */
 function getData(){
@@ -22,30 +55,48 @@ function getData(){
 	// Get data from element
 	var rawData = document.getElementById("rawdata").value;	
 	var dataArray = rawData.split(/\n/);
-	console.log(dataArray);
 	
 	// Separate date from temperature
 	for(var i = 0; i < dataArray.length; i++){
 		
 		var separateValues = dataArray[i].split(/,/);
-		console.log(separateValues[0]);
-		console.log(separateValues[1]);
-		
-		var string = separateValues[0];
-		
-		makeDate(string);
-		
 		separateValues[0] = new Date(makeDate(separateValues[0]));
 		separateValues[1] = parseInt(separateValues[1]);
-		
+        
+        // add time in milliseconds as well
+        separateValues[2] = separateValues[0].getTime();
+        
+        // and get the day offset based on these milliseconds
+        if(i > 0)
+            minimumTime = dataArray[0][2];
+        else 
+            minimumTime = separateValues[2];
+        
+        separateValues[3] = getDay(minimumTime, separateValues[2]);
+        
 		// Insert back into array as sub-array
 		dataArray[i] = separateValues;
 
 	}
-	
+    
+    // the full array with all data
 	console.log("after for loop" + dataArray);
+    
+    return dataArray;
 	
 }
+
+
+/* Function that creates day offset for every data point, taking the first point as day 1. */
+function getDay(minimumTime, dataPointTime){
+    
+   var DAY_IN_MILLISECONDS = 86400000;
+   var timeOffset = dataPointTime - minimumTime;
+   return (timeOffset / DAY_IN_MILLISECONDS) + 1;
+    
+}
+
+
 
 /* A small function that takes the date from the raw data and makes it suitable for the JS Date object. */
 function makeDate(dateString){
@@ -57,3 +108,44 @@ function makeDate(dateString){
 	return (dateString.substr(0,4) + "," + dateString.substr(4,2) + "," + dateString.substr(6,2));
 
 }
+
+
+function canvasTest(){
+    
+    var canvas = document.getElementById('mycanvas');
+    var context = canvas.getContext('2d');
+
+    context.beginPath();
+    context.moveTo(100, 150);
+    context.lineTo(450, 50);
+    context.stroke();
+      
+}
+
+
+function createTransform(domain, range){
+	// domain is a two-element array of the data bounds [domain_min, domain_max]
+	// range is a two-element array of the screen bounds [range_min, range_max]
+	// this gives you two equations to solve:
+	// range_min = alpha * domain_min + beta
+	// range_max = alpha * domain_max + beta
+    // a solution would be:
+
+    var domain_min = domain[0];
+    var domain_max = domain[1];
+    var range_min = range[0];
+    var range_max = range[1];
+
+    // formulas to calculate the alpha and the beta
+   	var alpha = (range_max - range_min) / (domain_max - domain_min);
+    var beta = range_max - alpha * domain_max;
+
+    console.log(alpha);
+    console.log(beta);
+    
+    // returns the function for the linear transformation (y= a * x + b)
+    return function(x){
+      return alpha * x + beta;
+    }
+}
+
