e3b0d559c0df3856a761c4ead85878f2d0830fce
Renske Talsma <vluuks@gmail.com>
2017-03-01 11:25:38 +0100

    Map works, color needs some work

--- Homework/week4/map.js
+++ Homework/week4/map.js
@@ -3,6 +3,9 @@
 	
 */
 
+/* Global stuff */
+var colors = [ "#ffffb2", "#fed976", "#feb24c", "#fd8d3c",  "#fc4e2a", "#e31a1c", "#b10026" ];
+
 /* Wait until page is ready. */
 $('document').ready(function(){
 	getJSONfile(doSomething);
@@ -21,6 +24,7 @@ function doSomething(data){
 }
 
 function theFantasticBeautifulAmazingMap(dataDictionary){
+	
 
 	console.log(document.getElementById('container'));
 
@@ -29,13 +33,23 @@ function theFantasticBeautifulAmazingMap(dataDictionary){
 		element: document.getElementById('container'),
 		
 		geographyConfig: {
-            highlightOnHover: true,
-            popupOnHover: true
+            popupTemplate: function(geo, data) {
+                return ['<div class="hoverinfo"><strong>',
+                        'Threatened plant species in ' + geo.properties.name,
+                        ': ' + data.Amount,
+                        '</strong></div>'].join('');
+            }
         },
 		
 		fills:{
-			TEST: 'red',
-			defaultFill: '#00446A' 
+			a: colors[0],
+			b: colors[1],
+			c: colors[2],
+			d: colors[3],
+			e: colors[4],
+			f: colors[5],
+			g: colors[6],
+			defaultFill: '#999999' 
 		},
 		
 		data: dataDictionary
@@ -47,22 +61,54 @@ function theFantasticBeautifulAmazingMap(dataDictionary){
 
 /* This function determines the fill key of an object based on the
 total amount of endangered plants. The more plants, the darker the color. */
-// function determineFillKey(){
+function determineFillKey(amount){
+		
+		console.log(amount);
+		console.log(0 < +amount <=10);
+		console.log(0 < amount && amount  <=10);
+		
+		if (amount == 0)
+			return "a";
+		else if(0 < amount && amount <= 10)
+			return "b";
+		else if(10 < amount && amount  <= 20)
+			return "c";
+		else if(20 < amount && amount  <= 30)
+			return "d";
+		else if(30 < amount && amount  <= 40)
+			return "e";
+		else if(40 < amount && amount  <= 50)
+			return "f";
+		else if(amount >= 50)
+			return "g";			
 
-// }
+}
+
+
+
+
 
 function mapDataToCountryCode(unmappedData){
 	
 	dataDictionary = {};
 	
+	// Create function to map colors to data
+	var paletteScale = d3.scale.linear()
+            .domain([0,2000]) // TODO MAGIC NUMBERS
+            .range(["#fee391","#b10026"])
+	
 	for (var i =0; i < unmappedData.length; i++){
 		
 		var key = unmappedData[i]['Country Code'];
-		unmappedData[i]['fillKey'] = "TEST";
+		var plantAmount = unmappedData[i]['Amount'];
+		unmappedData[i]['fillColor'] = paletteScale(+plantAmount);
 		dataDictionary[key] = unmappedData[i];
 	}
 	
 	console.log(dataDictionary);
+	
+	for( var key in dataDictionary)
+		console.log(dataDictionary[key].Amount);
 	
 	theFantasticBeautifulAmazingMap(dataDictionary);
 }
---
+++ Homework/week4/tutnotes.js
@@ -0,0 +1,18 @@
+ // We need to colorize every country based on "numberOfWhatever"
+    // colors should be uniq for every value.
+    // For this purpose we create palette(using min/max series-value)
+    var onlyValues = series.map(function(obj){ return obj[1]; });
+    var minValue = Math.min.apply(null, onlyValues),
+            maxValue = Math.max.apply(null, onlyValues);
+    // create color palette function
+    // color can be whatever you wish
+    var paletteScale = d3.scale.linear()
+            .domain([minValue,maxValue])
+            .range(["#EFEFFF","#02386F"]); // blue color
+    // fill dataset in appropriate format
+    series.forEach(function(item){ //
+        // item example value ["USA", 70]
+        var iso = item[0],
+                value = item[1];
+        dataset[iso] = { numberOfThings: value, fillColor: paletteScale(value) };
+    });
