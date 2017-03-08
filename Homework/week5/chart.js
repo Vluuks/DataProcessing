56db9f4cc2523da6fd5573bcb6e5ded79a7df4ae
Vluuks <vluuks@gmail.com>
2017-03-08 14:06:47 +0100

    Chart basic done

--- Homework/week5/chart.js
+++ Homework/week5/chart.js
@@ -6,64 +6,80 @@
 
 /* Wait until page is ready. */
 $('document').ready(function(){
-	initCanvas();
+    
+    // Load all of the data. 
+  
+        queue()
+            .defer(d3.json, 'Data/data2016.json')
+            .defer(d3.json, 'Data/data2015.json')
+            .await(initCanvas);
+        
 });
 
 
 
-function loadData(){
 
-	// get multiple files
-
-}
-
-
-function initCanvas(){
-		
-	var data = [{
-		"sale": "202",
-		"year": "2000"
-	}, {
-		"sale": "215",
-		"year": "2001"
-	}, {
-		"sale": "179",
-		"year": "2002"
-	}, {
-		"sale": "199",
-		"year": "2003"
-	}, {
-		"sale": "134",
-		"year": "2003"
-	}, {
-		"sale": "176",
-		"year": "2010"
-	}];
-
-	// Find chart svg
-	var svg = d3.select("#chart"),
-		width = 1000,
-		height = 500,
-		margins = {
-			top: 20,
-			right: 20,
-			bottom: 20,
-			left: 50
-		};
-		
-	var padding = 10;
+function initCanvas(error, data2016, data2015){
+    
+    console.log(data2016);
+    console.log(data2015);
+    
+    if(error)
+        throw error;
+    
+    // sample test data
+	var data = data2016;
+        
+    var margin = {top: 10, right: 10, bottom: 10, left: 25};
+    var width = 1900 - margin.left - margin.right,
+        height = 500 - margin.top - margin.bottom;   
+        
+    var svg = d3.select(".container")
+        .append("svg")
+        .attr("width", width + margin.left + margin.right)
+        .attr("height", height + margin.top + margin.bottom)
+        .append("g")
+        .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+		    
+    // Function to draw the line
+	var lineGen = d3.svg.line()
+		.x(function(d) {
+            console.log("date" + d.Date);
+            return xScale(makeDate(d.Date));
+		})
+		.y(function(d) {
+            console.log(d.Value);
+            return yScale(d.Value);
+		});
+    
+    
+    // Parse the data
+    data.columns = ["Date", "Avg", "High", "Low"];
+    var types = data.columns.slice(1).map(function(id) {
+        return {
+            id: id,
+            values: data.map(function(d) {
+                return {
+                    Date: d.Date, 
+                    Value: d[id]    
+                };
+            })
+        };
+    });
+  
+  
+    // Create scales	for X, Y and Z
+	var yScale = d3.scale.linear().range([height - margin.top, margin.bottom]).domain([0, 135]);
 	
-	// Create scales	for X and Y axes
-	var yScale = d3.scale.linear().range([height - margins.top, margins.bottom]).domain([134,215]);
-	
-	// Create X scale
-	// This will later depend on the chosen year
 	var mindate = new Date(2016,0,1),
 		maxdate = new Date(2016,11,31);
 		
 	var xScale = d3.time.scale()
 		.domain([mindate, maxdate])    // values between for month of january
-		.range([margins.left, width - margins.right]);   // map these the the chart width = total width minus padding at both sides
+		.range([margin.left, width - margin.right]);   // map these the the chart width = total width minus padding at both sides
+  
+    var zScale = d3.scale.ordinal(d3.schemeCategory10)
+        .domain(types.map(function(c) { return c.id; }));
   
 	// Create axes
 	var xAxis = d3.svg.axis()
@@ -73,64 +89,58 @@ function initCanvas(){
 	var yAxis = d3.svg.axis()
 		.scale(yScale)
 		.orient("left");
+    
+
 		
 	// Append to svg
 	svg.append("svg:g")
 		.attr("class","axis")
-		.attr("transform", "translate(0," + (height - margins.bottom) + ")")
+		.attr("transform", "translate(0," + (height - margin.bottom) + ")")
 		.call(xAxis);
 	
 	svg.append("svg:g")
 		.attr("class","axis")
-		.attr("transform", "translate(" + (margins.left) + ",0)")
-		.call(yAxis);
-	
-	// Function to draw the line
-	// I NEVER SAID WHICH DATA TO USE HOW DOES IT KNOW
-	var lineGen = d3.svg.line()
-		.x(function(d) {
-		return xScale(d.year);
-		})
-		.y(function(d) {
-		return yScale(d.sale);
-		});
-		
-	svg.append('svg:path')
-		.attr('d', lineGen(data))
-		.attr('stroke', 'green')
-		.attr('stroke-width', 2)
+		.attr("transform", "translate(" + (margin.left) + ",0)")
+		.call(yAxis)
+        .append("text")
+            .attr("x", -75 - margin.left)
+            .attr("y", 20)
+            .attr("transform", "rotate(-90)")
+             // ?????????
+            //.attr("x", 20)
+            //.attr("dy", "0.71em")
+            .attr("fill", "#000")
+            .text("Wind speed, m/s");
+                
+    var colors = {
+                            Avg: "#1b9e77",
+                            High: "#d95f02",
+                            Low: "#7570b3"
+                        }
+     
+     // Draw the line of doom
+     var type = svg.selectAll(".type")
+        .data(types)
+        .enter().append("g")
+        .attr("class", "type");
+        
+    type.append("path")
+        .attr("class", "line")
+        .attr("d", function(d) { return lineGen(d.values); })
+        .attr("stroke", function(d) { return colors[d.id]; })
+        //.attr('stroke', 'green')
+		.attr('stroke-width', 1)
 		.attr('fill', 'none');
-	
+        
 
-	
 }
 
 
 
+/* Takes the date from the raw data and makes it suitable for the JS Date object. */
+function makeDate(dateString){
 
+	dateString = dateString.trim();
+	return new Date((dateString.substr(0,4) + "," + dateString.substr(4,2) + "," + dateString.substr(6,2)));
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
